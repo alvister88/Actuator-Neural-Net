a1e4613bfbd7c8662ed7a8cdf28eb74725ebb63a
alvister88 <ayzhu2022@gmail.com>
2024-08-25 21:20:40 -0700

    Small updates to training and eval

--- actuator_nn/src/scripts/train.py
+++ actuator_nn/src/scripts/train.py
@@ -6,7 +6,7 @@ from ActuatorNetEvaluator import ActuatorNetEvaluator
 import wandb
 
 def main():
-    model_path = '../weights/best_actuator_model26.pt'
+    model_path = '../weights/best_actuator_model27.pt'
     train_data = '../data/train_data1.txt'
     validation_data = '../data/validation_data1.txt'
 
@@ -18,13 +18,13 @@ def main():
 
     # Set Wandb params
     project_name = 'actuator-net-training'
-    run_name = 'actuator-net-26'
+    run_name = 'actuator-net-27'
     entity_name = 'alvister88'
 
     # Train the model and get test data
     trained_model = trainer.train_model(
         train_data_path=train_data, val_data_path=validation_data,
-        lri=0.00005, lrf=0.000005, batch_size=32, patience=100, num_epochs=800, weight_decay=0.01,
+        lri=0.0001, lrf=0.00001, batch_size=32, patience=100, num_epochs=800, weight_decay=0.01,
         save_path=model_path, entity_name=entity_name, project_name=project_name, run_name=run_name
     )
 
