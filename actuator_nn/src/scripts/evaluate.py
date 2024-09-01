78ec4b0892bbcbf7b10d478262d794aa621d4f6c
alvister88 <ayzhu2022@gmail.com>
2024-08-31 21:55:35 -0700

    Added a notes text file to log some stuff

--- actuator_nn/src/scripts/evaluate.py
+++ actuator_nn/src/scripts/evaluate.py
@@ -12,7 +12,7 @@ import time
 def main():
     # Load the dataset
     data_path = '../data/gains2/torque_ripple1.txt'  # Update this path as needed
-    model_path = '../weights/best_actuator_gru_model29.pt'  # Update this path as needed
+    model_path = '../weights/best_actuator_gru_model30.pt'  # Update this path as needed
 
     evaluator = ActuatorNetEvaluator(model_path, run_device='cpu')
     
