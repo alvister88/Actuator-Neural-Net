Added data normalization to training, got rid of items with incorrect torque readings

actuator_nn/src/scripts/ActuatorNetEvaluator.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from ActuatorNet import ActuatorNet, HISTORY_SIZE, INPUT_SIZE, NUM_LAYERS
+from ActuatorNet import ActuatorNet, HISTORY_SIZE, INPUT_SIZE, NUM_LAYERS, MAX_TORQUE, MAX_VELOCITY, MAX_ERROR
 import time
 import os
 
@@ -21,7 +21,18 @@ class ActuatorNetEvaluator:
         torques = data['Torque'].values
         return position_errors, velocities, torques
 
+    def normalize_data(self, data, min_val, max_val):
+        return 2 * (data - min_val) / (max_val - min_val) - 1
+
+    def denormalize_torque(self, normalized_torque):
+        return (normalized_torque + 1) * (2 * MAX_TORQUE) / 2 - MAX_TORQUE
+
     def prepare_sequence_data(self, position_errors, velocities, torques):
+        # Normalize the data
+        position_errors = self.normalize_data(position_errors, -MAX_ERROR, MAX_ERROR)
+        velocities = self.normalize_data(velocities, -MAX_VELOCITY, MAX_VELOCITY)
+        torques = self.normalize_data(torques, -MAX_TORQUE, MAX_TORQUE)
+
         X, y = [], []
         for i in range(len(torques) - self.history_size + 1):
             X.append(np.column_stack((position_errors[i:i+self.history_size], 

actuator_nn/src/scripts/ActuatorNetPredictor.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 from collections import deque
-from ActuatorNet import ActuatorNet, INPUT_SIZE
+from ActuatorNet import ActuatorNet, INPUT_SIZE, MAX_TORQUE, MAX_VELOCITY, MAX_ERROR
 
 class ActuatorNetPredictor:
     def __init__(self, model_path, device=None):
@@ -12,25 +12,28 @@ class ActuatorNetPredictor:
 
     def load_model(self, model_path):
         state_dict = torch.load(model_path, map_location=self.device, weights_only=True)
-        
-        # Determine the hidden size and number of layers from the state dict
         hidden_size = state_dict['gru.weight_ih_l0'].size(0) // 3
         num_layers = sum(1 for key in state_dict.keys() if key.startswith('gru.weight_ih_l'))
-        
-        # Create the model with the correct architecture
         model = ActuatorNet(hidden_size=hidden_size, num_layers=num_layers, dropout_rate=0.1)
         model.load_state_dict(state_dict)
         model.to(self.device)
         model.eval()
-        
-        # Determine the history size (which should be equal to the hidden size in this case)
         history_size = hidden_size
-        
         return model, history_size
 
+    def normalize_data(self, data, min_val, max_val):
+        return 2 * (data - min_val) / (max_val - min_val) - 1
+
+    def denormalize_torque(self, normalized_torque):
+        return (normalized_torque + 1) * (2 * MAX_TORQUE) / 2 - MAX_TORQUE
+
     def prepare_input(self, error, velocity):
-        self.error_buffer.append(error)
-        self.velocity_buffer.append(velocity)
+        # Normalize the input
+        normalized_error = self.normalize_data(error, -MAX_ERROR, MAX_ERROR)
+        normalized_velocity = self.normalize_data(velocity, -MAX_VELOCITY, MAX_VELOCITY)
+
+        self.error_buffer.append(normalized_error)
+        self.velocity_buffer.append(normalized_velocity)
 
         # Pad the buffers if they're not full
         padded_errors = list(self.error_buffer) + [0] * (self.history_size - len(self.error_buffer))
@@ -40,14 +43,14 @@ class ActuatorNetPredictor:
             np.array(padded_errors),
             np.array(padded_velocities)
         ))
-
         return torch.FloatTensor(input_data).unsqueeze(0).to(self.device)
 
     def predict(self, error, velocity):
         input_tensor = self.prepare_input(error, velocity)
         with torch.no_grad():
             prediction = self.model(input_tensor)
-        return prediction.item()
+        # Denormalize the prediction
+        return self.denormalize_torque(prediction.item())
 
 # Example usage
 def main():

actuator_nn/src/scripts/ActuatorNetTrainer.py
@@ -6,7 +6,7 @@ import pandas as pd
 import os
 import wandb
 import time
-from ActuatorNet import ActuatorNet, HISTORY_SIZE, INPUT_SIZE, NUM_LAYERS
+from ActuatorNet import ActuatorNet, HISTORY_SIZE, INPUT_SIZE, NUM_LAYERS, MAX_TORQUE, MAX_VELOCITY, MAX_ERROR
 import signal
 
 class ActuatorNetTrainer:
@@ -41,10 +41,22 @@ class ActuatorNetTrainer:
                                       velocities[i:i+HISTORY_SIZE])))
             y.append(torques[i+HISTORY_SIZE-1])
         return np.array(X), np.array(y)
+    
+    def normalize_data(self, data, min_val, max_val):
+        return 2 * (data - min_val) / (max_val - min_val) - 1
 
     def prepare_data(self, train_data_path, val_data_path):
         train_position_errors, train_velocities, train_torques = self.load_data(train_data_path)
         val_position_errors, val_velocities, val_torques = self.load_data(val_data_path)
+
+        # Normalize the data
+        train_position_errors = self.normalize_data(train_position_errors, -MAX_ERROR, MAX_ERROR)
+        train_velocities = self.normalize_data(train_velocities, -MAX_VELOCITY, MAX_VELOCITY)
+        train_torques = self.normalize_data(train_torques, -MAX_TORQUE, MAX_TORQUE)
+
+        val_position_errors = self.normalize_data(val_position_errors, -MAX_ERROR, MAX_ERROR)
+        val_velocities = self.normalize_data(val_velocities, -MAX_VELOCITY, MAX_VELOCITY)
+        val_torques = self.normalize_data(val_torques, -MAX_TORQUE, MAX_TORQUE)
 
         X_train, y_train = self.prepare_sequence_data(train_position_errors, train_velocities, train_torques)
         X_val, y_val = self.prepare_sequence_data(val_position_errors, val_velocities, val_torques)
@@ -55,6 +67,9 @@ class ActuatorNetTrainer:
         y_val = torch.FloatTensor(y_val).to(self.device)
 
         return X_train, y_train, X_val, y_val
+
+    def denormalize_torque(self, normalized_torque):
+        return (normalized_torque + 1) * (2 * MAX_TORQUE) / 2 - MAX_TORQUE
 
     def setup_training(self, lri, lrf, weight_decay, num_epochs):
         criterion = nn.MSELoss()

actuator_nn/src/scripts/train.py
@@ -8,10 +8,10 @@ import wandb
 
 def main():
     # Set paths
-    model_path = '../weights/best_actuator_gru_model37.pt'
-    train_data = '../data/gains2/train_data_3.txt'
-    validation_data = '../data/gains2/validation_data_3.txt'
-    eval_data_path = '../data/gains2/test1.txt'
+    model_path = '../weights/actuator_gruv2_model1.pt'
+    train_data = '../data/gains3/train_data_3.txt'
+    validation_data = '../data/gains3/validation_data_3.txt'
+    eval_data_path = '../data/gains3/test1.txt'
 
     # Set device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -20,11 +20,11 @@ def main():
     # model = ActuatorNet(hidden_size=HISTORY_SIZE, num_layers=NUM_LAYERS, dropout_rate=0.1)
 
     # Create the trainer
-    trainer = ActuatorNetTrainer(hidden_size=HISTORY_SIZE, num_layers=NUM_LAYERS, dropout_rate=0.01, device=device)
+    trainer = ActuatorNetTrainer(hidden_size=HISTORY_SIZE, num_layers=NUM_LAYERS, dropout_rate=0.03, device=device)
 
     # Set Wandb params
     project_name = 'actuator-net-training'
-    run_name = 'actuator-net-gru-37'
+    run_name = 'actuator-net-gruv2-1'
     entity_name = 'alvister88'
 
     # Train the model