54e03f4e2927123285dabe422826b73bd486b122
alvister88 <alvinzhu2022@gmail.com>
2024-09-26 01:21:55 -0700

    Added train.py script to use yaml for configs

--- actuator_nn/src/scripts/train.py
+++ actuator_nn/src/scripts/train.py
@@ -1,44 +1,55 @@
 import numpy as np
 import pandas as pd
 import torch
+import yaml
 from ActuatorNet import ActuatorNet, HISTORY_SIZE, NUM_LAYERS
 from ActuatorNetTrainer import ActuatorNetTrainer
 from ActuatorNetEvaluator import ActuatorNetEvaluator
 import wandb
 
+def load_config(config_path):
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
 def main():
+    # Load configuration
+    config = load_config('../configs/train_config.yaml')
+
     # Set paths
-    model_path = '../weights/actuator_gruv3_model15.pt'
-    train_data = '../data/gains3/train_LP2.txt'
-    validation_data = '../data/gains3/valid_LP2.txt'
-    eval_data_path = '../data/gains3/test3.txt'
+    model_path = config['model']['path']
+    train_data = config['data']['train_data_path']
+    validation_data = config['data']['validation_data_path']
+    eval_data_path = config['data']['eval_data_path']
 
     # Set device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # # Create the model
-    # model = ActuatorNet(hidden_size=HISTORY_SIZE, num_layers=NUM_LAYERS, dropout_rate=0.1)
+    device = torch.device(config['device'] if torch.cuda.is_available() else "cpu")
 
     # Create the trainer
-    trainer = ActuatorNetTrainer(hidden_size=HISTORY_SIZE, num_layers=NUM_LAYERS, dropout_rate=0.03, device=device)
+    trainer = ActuatorNetTrainer(
+        hidden_size=config['model']['hidden_size'], 
+        num_layers=config['model']['num_layers'], 
+        dropout_rate=config['model']['dropout_rate'], 
+        device=device
+    )
 
     # Set Wandb params
-    project_name = 'actuator-net-training-v3'
-    run_name = 'actuator-net-gruv3-15'
-    entity_name = 'alvister88'
+    project_name = config['wandb']['project_name']
+    run_name = config['wandb']['run_name']
+    entity_name = config['wandb']['entity_name']
 
     # Train the model
     trained_model = trainer.train_model(
         train_data_path=train_data,
         val_data_path=validation_data,
-        lri=0.0001,
-        lrf=0.000005,
-        batch_size=64,
-        patience=400,
-        num_epochs=3500,
-        pct_start=0.12,
-        anneal_strategy='cos',
-        weight_decay=0.01,
+        lri=config['training']['lri'],
+        lrf=config['training']['lrf'],
+        batch_size=config['training']['batch_size'],
+        patience=config['training']['patience'],
+        num_epochs=config['training']['num_epochs'],
+        pct_start=config['training']['pct_start'],
+        anneal_strategy=config['training']['anneal_strategy'],
+        weight_decay=config['training']['weight_decay'],
         save_path=model_path,
         entity_name=entity_name,
         project_name=project_name,
